d7c3f402e68b74168ed787bd17fd083ce31403e5
Jeremiah Scanlon <jeremiah.j.scanlon@gmail.com>
2023-09-20 23:26:32 -0400

    skipping scss

--- client/app/index.js
+++ client/app/index.js
@@ -15,7 +15,7 @@ import Home from './components/Home/Home';
 
 import HelloWorld from './components/HelloWorld/HelloWorld';
 
-import './styles/styles.scss';
+import './styles/vendor/normalize.css';
 
 render((
   <Router>
